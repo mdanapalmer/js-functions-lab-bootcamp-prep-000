066eb5e9925d4b8404916c2d1b2d5f1f77f085a4
Dana Palmer <roiai4@hotmail.com>
2018-04-24 02:53:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function happyHolidays()
 function happyHolidaysTo(name) 
 { return `Happy holidays, ${name}!` }
 
-function happyHolidayTo(name)
-{ 
+function happyHolidayTo(holiday, name) 
+{ return `Happy ${holiday}, ${name}!` }
